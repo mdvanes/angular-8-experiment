47da0cff0d03865bc66dcf3024d9de49dafc9c48
M.D. van Es <es@mdworld.nl>
2020-03-18 17:56:03 +0100

    Fix unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,22 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+
+// https://angular.io/guide/testing#stubbing-unneeded-components
+
+@Component({selector: 'app-heroes', template: ''})
+class HeroesStubComponent {}
+
+@Component({selector: 'app-messages', template: ''})
+class MessagesStubComponent {}
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HeroesStubComponent,
+        MessagesStubComponent
       ],
     }).compileComponents();
   }));
@@ -16,16 +27,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-tour-of-heroes'`, () => {
+  it(`should have as title 'Tour of Heroes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-tour-of-heroes');
+    expect(app.title).toEqual('Tour of Heroes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-tour-of-heroes app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Tour of Heroes');
   });
 });
