f935efa1b01df4e63a1f03b8369696fe584bf692
M.D. van Es <es@mdworld.nl>
2020-03-18 17:44:05 +0100

    Add services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { delay, timeout } from 'rxjs/operators';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,22 +10,36 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
   }
-  constructor() { }
+
+  getHeroes(): void /* async getHeroes(): Promise<void> */ {
+    // const foo = this.heroService.getHeroes();
+    // console.log(foo);
+    // this.heroes = foo;
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+    // const foo = await this.heroService.getHeroes();
+    // foo.subscribe(heroes => this.heroes = heroes);
+  }
+
+  // DI param is automatically assigned to member with the same name
+  constructor(private heroService: HeroService/*, private myTestParam: string = 'bla'*/, private messageService: MessageService) { }
 
   ngOnInit() {
-
+    this.getHeroes();
+    // console.log(this.myTestParam);
   }
 
 }
