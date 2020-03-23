d3986f36a4e782bd060b798635627430af337c3e
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-03-23 11:34:12 +0100

    Add example of class constructor shadowing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -4,6 +4,27 @@ import { HeroService } from '../hero.service';
 import { delay, timeout } from 'rxjs/operators';
 import { MessageService } from '../message.service';
 
+// Test "auto assigning" of constructor parameters to members
+class MyTest {
+  // requires TS 3.8+: #barESPrivate: string;
+  private barDefault: string;
+
+  // public by default
+  constructor(private barPrivate, public barPublic, barDefault) {
+    // It is not needed to do this.barPrivate = barPrivate or for public, but it is needed for default.
+    this.barDefault = barDefault;
+  }
+
+  getMembers(): Record<string, string> {
+    return {
+      // barESPrivate: this.barESPrivate,
+      barPrivate: this.barPrivate,
+      barPublic: this.barPublic,
+      barDefault: this.barDefault
+    };
+  }
+}
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -40,6 +61,13 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
     this.getHeroes();
     // console.log(this.myTestParam);
+    const myTest = new MyTest('BAR_PRIVATE', 'BAR_PUBLIC', 'BAR_DEFAULT');
+    console.log(`myTest members: ${JSON.stringify(myTest.getMembers())} | barPublic: ${myTest.barPublic} | members as obj:`
+      , myTest.getMembers());
+    // Causes compile error, but works runtime:
+    // console.log(`myTest barPrivate: ${myTest.barPrivate} | barDefault: ${myTest.barDefault}`);
+    // TS 3.8+ causes compile error AND runtime error
+    // console.log(`myTest barESPrivate: ${myTest.barESPrivate}`);
   }
 
 }
