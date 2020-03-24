ff0d7805a0750a9ea78921d7a021437311708f8d
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-03-24 12:19:29 +0100

    Add notification for testing nested component props

--- src/my-project-name/my-foo/my-foo.component.spec.ts
+++ src/my-project-name/my-foo/my-foo.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MyFooComponent } from './my-foo.component';
+import { Component, Input } from '@angular/core';
+import { Hero } from '../../app/hero';
+import { By } from '@angular/platform-browser';
+
+@Component({selector: 'app-notification', template: ''})
+class NotificationStubComponent {
+  @Input() errorMessage: string;
+}
 
 describe('MyFooComponent', () => {
   let component: MyFooComponent;
@@ -8,7 +16,7 @@ describe('MyFooComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MyFooComponent ]
+      declarations: [ MyFooComponent, NotificationStubComponent ]
     })
     .compileComponents();
   }));
@@ -30,13 +38,28 @@ describe('MyFooComponent', () => {
 
   describe('with errorMessage attribute', () => {
       beforeEach(() => {
-        component.errorMessage = 'my test error message!';
+        component.errorMessage = 'My Test Error Message!';
         fixture.detectChanges();
       });
 
       it('has an error message if the attribute is set', () => {
         const h1Content = fixture.debugElement.nativeElement.querySelector('h1').textContent;
-        expect(h1Content).toContain('MY TEST ERROR MESSAGE!');
+        expect(h1Content).toContain('my test error message!');
+      });
+
+      // TODO do this without the ng-reflect- prefix
+      it('has an "notification" component with an "errormessage" attribute', () => {
+        // const notificationElem = fixture.debugElement.query(By.directive(NotificationStubComponent));
+        // expect(notificationElem.errorMessage).toContain('MY TEST ERROR MESSAGE!');
+        const notificationElem = fixture.debugElement.nativeElement.querySelector('app-notification');
+        // expect(notificationElem.attributes).toEqual({errorMessage: 'MY TEST ERROR MESSAGE!'});
+        // const x = notificationElem.attributes.find(attr => attr.name === 'ng-reflect-error-message').textContent;
+        // console.log(notificationElem.attributes, notificationElem.attributes.getNamedItem('ng-reflect-error-message'));
+        const attr = notificationElem.attributes.getNamedItem('ng-reflect-error-message');
+        // console.log(attr.textContent);
+        expect(attr.textContent).toBe('My Test Error Message!');
+        // expect(notificationElem.attributes[1].value).toBe('My Test Error Message!');
+        // expect(notificationElem.attributes).toContain('My Test Error Message!');
       });
   });
 });
