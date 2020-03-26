1644bc020ff89d5892a020f9bf2dbea516d01a7b
M.D. van Es <es@mdworld.nl>
2020-03-26 14:42:57 +0100

    Add formArray

--- src/my-project-name/reactive-form/reactive-form.component.ts
+++ src/my-project-name/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators, FormArray, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -19,14 +19,53 @@ export class ReactiveFormComponent {
 
   colorForm = new FormGroup({
     color4Control: new FormControl('', Validators.required),
+    // It is also possible to set disabled like this: color4Control: new FormControl({value: 'red', disabled: true}, Validators.required),
     color5Control: new FormControl('red')
   });
 
   colorBuildForm = this.fb.group({
     color6Control: ['', Validators.required],
     color7Control: ['', Validators.required]
+    /*
+    // Nested is also allowed:
+    specialColors: this.fb.group([])
+    */
+  });
+
+  colorBuildArrayGroup = new FormGroup({
+    colorBuildArray: this.fb.array([
+      this.fb.control('', Validators.required)
+    ])
   });
 
   constructor(private fb: FormBuilder) {}
+
+  // // Untested
+  // get color6() {
+  //   return this.colorBuildForm.get('color6Control').value;
+  // }
+  //
+  // // Untested
+  // get specialColors() {
+  //   return this.colorBuildForm.get('specialColors') as FormGroup;
+  // }
+
+  addGroupColor() {
+    // This can also be done on a FormGroup with .addControl, but that requires a control name each time
+    this.colorBuildForm.addControl('color8Control', new FormControl('', Validators.required));
+  }
+
+  hasColor8Control(): boolean {
+    return Boolean(this.colorBuildForm.get('color8Control'));
+  }
+
+  get colorBuildArray() {
+    return this.colorBuildArrayGroup.get('colorBuildArray') as FormArray;
+  }
+
+  addArrayColor() {
+    // This can also be done on a FormGroup with .addControl, but that requires a control name each time
+    this.colorBuildArray.push(this.fb.control('', Validators.required));
+  }
 
 }
