43cb5866fb2f4dbba84aaf53add2dea62ee7c6c1
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-03-27 10:52:45 +0100

    Fix heroservice unit test

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing';
-
 import { HeroService } from './hero.service';
+import { HttpClient } from '@angular/common/http';
+import { MessageService } from './message.service';
 
 describe('HeroService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    const HttpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post', 'put', 'delete']);
+    const MessageServiceSpy = jasmine.createSpyObj('MessageService', ['add']);
+    TestBed.configureTestingModule({ providers: [ HeroService,
+        { provide: HttpClient, useValue: HttpClientSpy },
+        { provide: MessageService, useValue: MessageServiceSpy } ] });
+  });
 
   it('should be created', () => {
     const service: HeroService = TestBed.get(HeroService);
     expect(service).toBeTruthy();
+  });
+
+  it('should call message service', () => {
+    const service: HeroService = TestBed.get(HeroService);
+    service.testLog();
+    const spy = TestBed.get(MessageService);
+    expect(spy.add.calls.count()).toBe(1);
   });
 });
