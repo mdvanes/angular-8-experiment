575302e265f3757673d7e1ae133664558b4a716a
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-03-27 15:13:03 +0100

    Add e2e with debugging

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, by, element, ElementArrayFinder, logging } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -10,7 +10,32 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-tour-of-heroes app is running!');
+    expect(page.getTitleText()).toEqual('Tour of Heroes');
+  });
+
+  it('navigates to details after clicking on first top hero', async () => {
+    await page.navigateTo();
+    // const attr = await element.all(by.css('app-dashboard a')).get(1).getAttribute('href'); // .then(y => console.log(y));
+    // console.log(attr);
+    // const x = await page.getTopHeroPanels() as unknown as ElementArrayFinder;
+    // console.log(x.count());
+
+    // Does not work anymore since Node 8:
+    // browser.pause();
+
+    await page.clickTopHeroPanel(14);
+    debugger;
+    // expect(x).toEqual([]);
+    expect(page.getHeroDetailTitleText()).toEqual('CELERITAS Details');
+    // TODO fix so it will also succeed when running with `ng e2e` -> this is because ng e2e was not running on port 4200, so the string compare fails.
+    // TODO add cucumber
+
+    // To debug:
+    // 1. Add a `debugger;` statement somewhere in the code
+    // 2. do not start with `ng e2e` but with `ng serve` and in other terminal `npm run e2e:debug`
+    // 3. In Chrome, open the console and click the green "NodeJS" icon
+    // 4. A new Chrome window will open. Click the blue `continue` arrow.
+    // 5. The tests will start running. The next breakpoint will be at the `debugger;` statement.
   });
 
   afterEach(async () => {
