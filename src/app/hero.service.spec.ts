a761c89398f58f87348e51f5daffa83c8acbe12a
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2020-03-30 14:36:49 +0200

    Fix unit tests

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -2,10 +2,13 @@ import { TestBed } from '@angular/core/testing';
 import { HeroService } from './hero.service';
 import { HttpClient } from '@angular/common/http';
 import { MessageService } from './message.service';
+import { Hero } from './hero';
 
 describe('HeroService', () => {
+  let HttpClientSpy;
+
   beforeEach(() => {
-    const HttpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post', 'put', 'delete']);
+    HttpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post', 'put', 'delete']);
     const MessageServiceSpy = jasmine.createSpyObj('MessageService', ['add']);
     TestBed.configureTestingModule({ providers: [ HeroService,
         { provide: HttpClient, useValue: HttpClientSpy },
@@ -22,5 +25,34 @@ describe('HeroService', () => {
     service.testLog();
     const spy = TestBed.get(MessageService);
     expect(spy.add.calls.count()).toBe(1);
+  });
+
+  it('performs GET on heroesUrl for getHero', () => {
+    const service: HeroService = TestBed.get(HeroService);
+    HttpClientSpy.get.and.returnValue({ pipe: jasmine.createSpy() } );
+    service.getHero(42);
+    const spy = TestBed.get(HttpClient);
+    expect(spy.get.calls.count()).toBe(1);
+    expect(spy.get.calls.argsFor(0)).toEqual(['api/heroes/42']);
+  });
+
+  it('performs GET on heroesUrl for getHeroes', () => {
+    const service: HeroService = TestBed.get(HeroService);
+    HttpClientSpy.get.and.returnValue({ pipe: jasmine.createSpy() } );
+    service.getHeroes();
+    const spy = TestBed.get(HttpClient);
+    expect(spy.get.calls.count()).toBe(1);
+    expect(spy.get.calls.argsFor(0)).toEqual(['api/heroes']);
+  });
+
+  it('performs PUT on heroesUrl for updateHero', () => {
+    const exampleHero: Hero = { id: 42, name: 'Towel Man' };
+    const service: HeroService = TestBed.get(HeroService);
+    HttpClientSpy.put.and.returnValue({ pipe: jasmine.createSpy() } );
+    service.updateHero(exampleHero);
+    const spy = TestBed.get(HttpClient);
+    expect(spy.put.calls.count()).toBe(1);
+    expect(spy.put.calls.argsFor(0)[0]).toBe('api/heroes');
+    expect(spy.put.calls.argsFor(0)[1]).toEqual(exampleHero);
   });
 });
