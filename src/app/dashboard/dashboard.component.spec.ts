8d167b7772c424d04cb7c6fba5ffc298692dfdc9
M.D. van Es <es@mdworld.nl>
2020-03-30 13:07:51 +0200

    Fix dashboard unit test

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterLinkDirectiveStub } from '../../testing/router-link-directive-stub';
 
 import { DashboardComponent } from './dashboard.component';
+import { Component } from '@angular/core';
+import { HeroService } from '../hero.service';
+
+@Component({selector: 'app-hero-search', template: ''})
+class HeroSearchStubComponent {}
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async(() => {
+    const HeroServiceSpy = jasmine.createSpyObj('HeroService', ['getHeroes']);
+    HeroServiceSpy.getHeroes.and.returnValue({ subscribe: jasmine.createSpy() } );
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent, RouterLinkDirectiveStub, HeroSearchStubComponent ],
+      providers: [ DashboardComponent,
+        { provide: HeroService, useValue: HeroServiceSpy }
+      ]
     })
     .compileComponents();
   }));
