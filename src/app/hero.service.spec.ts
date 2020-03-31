a5baa68c40287f690b2de2e16205c891e911c57d
M.D. van Es <es@mdworld.nl>
2020-03-31 10:30:00 +0200

    Add unit tests for HeroService methods

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -55,4 +55,42 @@ describe('HeroService', () => {
     expect(spy.put.calls.argsFor(0)[0]).toBe('api/heroes');
     expect(spy.put.calls.argsFor(0)[1]).toEqual(exampleHero);
   });
+
+  it('performs POST on heroesUrl for addHero', () => {
+    const exampleHero: Hero = { id: 42, name: 'Towel Man' };
+    const service: HeroService = TestBed.get(HeroService);
+    HttpClientSpy.post.and.returnValue({ pipe: jasmine.createSpy() } );
+    service.addHero(exampleHero);
+    const spy = TestBed.get(HttpClient);
+    expect(spy.post.calls.count()).toBe(1);
+    expect(spy.post.calls.argsFor(0)[0]).toBe('api/heroes');
+    expect(spy.post.calls.argsFor(0)[1]).toEqual(exampleHero);
+  });
+
+  it('performs DELETE on heroesUrl for deleteHero', () => {
+    const exampleHero: Hero = { id: 42, name: 'Towel Man' };
+    const service: HeroService = TestBed.get(HeroService);
+    HttpClientSpy.delete.and.returnValue({ pipe: jasmine.createSpy() } );
+    service.deleteHero(exampleHero);
+    const spy = TestBed.get(HttpClient);
+    expect(spy.delete.calls.count()).toBe(1);
+    expect(spy.delete.calls.argsFor(0)[0]).toBe('api/heroes/42');
+  });
+
+  it('performs GET on heroesUrl for searchHeroes', () => {
+    const service: HeroService = TestBed.get(HeroService);
+    HttpClientSpy.get.and.returnValue({ pipe: jasmine.createSpy() } );
+    service.searchHeroes('Towel');
+    const spy = TestBed.get(HttpClient);
+    expect(spy.get.calls.count()).toBe(1);
+    expect(spy.get.calls.argsFor(0)).toEqual(['api/heroes/?name=Towel']);
+  });
+
+  it('does not perform GET on heroesUrl for searchHeroes with empty term', () => {
+    const service: HeroService = TestBed.get(HeroService);
+    HttpClientSpy.get.and.returnValue({ pipe: jasmine.createSpy() } );
+    service.searchHeroes('');
+    const spy = TestBed.get(HttpClient);
+    expect(spy.get.calls.count()).toBe(0);
+  });
 });
